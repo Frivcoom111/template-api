c7f59db0216ee6ed26872641f32a3be30da6e713
Frivcoom111 <carloseduardofaustino67@gmail.com>
2026-05-18 20:17:58 -0300

    Funcionalidade: integração do Cloudinary e do Multer para o gerenciamento de uploads de arquivos.

---
+++ src/lib/multer.ts
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    },
+    fileFilter: (req, file, cb) => {
+        file.mimetype
+    }
+})
+
+export default upload
